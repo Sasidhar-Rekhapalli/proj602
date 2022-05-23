49b05e376976f19c07f077594e95f52075f2f5cf
Moises Melgarejo <melgarejoaguilar5636@saskpolytech.ca>
2022-05-23 16:24:59 +0000

    Added changes for server.js.

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,16 @@
-"Backend!";
+const express = require('express');
+const backend = express();
+const db = require('./db/connect');
+const router = require('./routes/route');
+
+backend.use(express.json());
+
+backend.get('/', function(req, res){
+    res.redirect("/isms/main");
+});
+
+backend.use('/isms', router);
+
+backend.listen(3000, ()=>{
+    console.log('Server is running');
+});
