10df3bdc81512e51faac6c9f1b050bb229cbd6b2
Nilpatel <patel9462@saskpolytech.ca>
2022-05-25 09:11:37 -0600

    basic layout done login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import AddUser from "./components/AddUser";
 import Reset from "./components/ResetPassword";
+import Login from './components/Login';
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
