c19e5c700f525abba36d73c8f17102c779feecb2
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-25 08:36:35 -0600

    Get User By ID route

--- back-end/controllers/controller.js
+++ back-end/controllers/controller.js
@@ -1,128 +1,145 @@
-const dbObject = require('../db/connect');
-
-getAllStudents = async(req, res) =>{
-    dbObject.getConnection((err, connection) =>{
-        connection.query('SELECT * FROM student', (err, rows)=>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success: false, error: err});
-            }
-            return res.status(200).json({success: true, data: rows});
-        });
+const dbObject = require("../db/connect");
+/////////////         GET ALL STUDENTs         /////////////
+getAllStudents = async (req, res) => {
+  dbObject.getConnection((err, connection) => {
+    connection.query("SELECT * FROM student", (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ success: true, data: rows });
     });
-}
-
-getAllUsers = async(req, res) =>{
-    dbObject.getConnection((err, connection) =>{
-        connection.query('SELECT * FROM user', (err, rows)=>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success: false, error: err});
-            }
-            return res.status(200).json({success: true, data: rows});
-        });
+  });
+};
+/////////////         GET ALL USERS         /////////////
+getAllUsers = async (req, res) => {
+  dbObject.getConnection((err, connection) => {
+    connection.query("SELECT * FROM user", (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ success: true, data: rows });
     });
-}
-
-
-getStudentById = async(req, res) =>{
-    var studentId = req.params.id;
-    var sql = "SELECT * FROM student WHERE student_id = ?";
-    dbObject.getConnection((err, connection) =>{
-        connection.query(sql, studentId, (err, rows)=>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success: false, error: err});
-            }
-            return res.status(200).json({success: true, data: rows});
-        });
+  });
+};
+/////////////         GET  STUDENT BY ID        /////////////
+getStudentById = async (req, res) => {
+  var studentId = req.params.id;
+  var sql = "SELECT * FROM student WHERE student_id = ?";
+  dbObject.getConnection((err, connection) => {
+    connection.query(sql, studentId, (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ success: true, data: rows });
     });
-}
-
-createStudent = async(req, res) => {
-    var sql = "INSERT INTO student (prospective, std_id, first_name, middle_name, last_name, gender, birthdate, email, country, academic_period, campus, program, degree, year, graudate_ind, enroll) VALUES ?";
-    var values = [
-        [
-            req.body.prospective, 
-            req.body.std_id, 
-            req.body.first_name, 
-            req.body.middle_name, 
-            req.body.last_name, 
-            req.body.gender, 
-            req.body.birthdate, 
-            req.body.email, 
-            req.body.country, 
-            req.body.academic_period, 
-            req.body.campus, 
-            req.body.program, 
-            req.body.degree, 
-            req.body.year, 
-            req.body.graudate_ind, 
-            req.body.enroll
-        ]
-    ];
-    dbObject.getConnection((err, connection) =>{        
-        connection.query(sql, [values], (err, rows) =>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success:false, error: err});
-            }
-            return res.status(200).json({message: "Student Created" });
-        })
+  });
+};
+/////////////         GET  USER BY ID        /////////////
+getUserById = async (req, res) => {
+  var studentId = req.params.id;
+  var sql = "SELECT * FROM user WHERE user_id = ?";
+  dbObject.getConnection((err, connection) => {
+    connection.query(sql, studentId, (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ success: true, data: rows });
     });
-}
-
-updateStudent = async(req, res) => {
-    var studentId = req.params.id;
-    var sql = "UPDATE student SET prospective = ?, std_id = ?, first_name = ?, middle_name = ?, last_name = ?, gender = ?, birthdate = ?, email = ?, country = ?, academic_period = ?, campus = ?, program = ?, degree = ?, year = ?, graudate_ind = ?, enroll = ? WHERE student_id = " + studentId;
-    var values = [
-            req.body.prospective, 
-            req.body.std_id, 
-            req.body.first_name, 
-            req.body.middle_name, 
-            req.body.last_name, 
-            req.body.gender, 
-            req.body.birthdate, 
-            req.body.email, 
-            req.body.country, 
-            req.body.academic_period, 
-            req.body.campus, 
-            req.body.program, 
-            req.body.degree, 
-            req.body.year, 
-            req.body.graudate_ind, 
-            req.body.enroll
-    ];
-    dbObject.getConnection((err, connection) =>{
-        connection.query(sql, values, (err, rows) =>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success:false, error: err});
-            }
-            return res.status(200).json({message: "Student Updated" });
-        })
+  });
+};
+/////////////         CREATE STUDENT         /////////////
+createStudent = async (req, res) => {
+  var sql =
+    "INSERT INTO student (prospective, std_id, first_name, middle_name, last_name, gender, birthdate, email, country, academic_period, campus, program, degree, year, graudate_ind, enroll) VALUES ?";
+  var values = [
+    [
+      req.body.prospective,
+      req.body.std_id,
+      req.body.first_name,
+      req.body.middle_name,
+      req.body.last_name,
+      req.body.gender,
+      req.body.birthdate,
+      req.body.email,
+      req.body.country,
+      req.body.academic_period,
+      req.body.campus,
+      req.body.program,
+      req.body.degree,
+      req.body.year,
+      req.body.graudate_ind,
+      req.body.enroll,
+    ],
+  ];
+  dbObject.getConnection((err, connection) => {
+    connection.query(sql, [values], (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ message: "Student Created" });
     });
-}
+  });
+};
 
-deleteStudent = async(req, res) => {
-    var studentId = req.params.id;
-    var sql = "DELETE FROM student WHERE student_id = ?";
-    dbObject.getConnection((err, connection) =>{
-        connection.query(sql, studentId, (err, rows) =>{
-            connection.release();
-            if(err){
-                return res.status(400).json({success:false, error: err});
-            }
-            return res.status(200).json({message: "Student Deleted" });
-        })
+updateStudent = async (req, res) => {
+  var studentId = req.params.id;
+  var sql =
+    "UPDATE student SET prospective = ?, std_id = ?, first_name = ?, middle_name = ?, last_name = ?, gender = ?, birthdate = ?, email = ?, country = ?, academic_period = ?, campus = ?, program = ?, degree = ?, year = ?, graudate_ind = ?, enroll = ? WHERE student_id = " +
+    studentId;
+  var values = [
+    req.body.prospective,
+    req.body.std_id,
+    req.body.first_name,
+    req.body.middle_name,
+    req.body.last_name,
+    req.body.gender,
+    req.body.birthdate,
+    req.body.email,
+    req.body.country,
+    req.body.academic_period,
+    req.body.campus,
+    req.body.program,
+    req.body.degree,
+    req.body.year,
+    req.body.graudate_ind,
+    req.body.enroll,
+  ];
+  dbObject.getConnection((err, connection) => {
+    connection.query(sql, values, (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ message: "Student Updated" });
     });
-}
+  });
+};
+
+deleteStudent = async (req, res) => {
+  var studentId = req.params.id;
+  var sql = "DELETE FROM student WHERE student_id = ?";
+  dbObject.getConnection((err, connection) => {
+    connection.query(sql, studentId, (err, rows) => {
+      connection.release();
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      return res.status(200).json({ message: "Student Deleted" });
+    });
+  });
+};
 
 module.exports = {
-    getAllStudents,
-    getStudentById,
-    createStudent,
-    updateStudent,
-    deleteStudent,
-    getAllUsers
-}
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+  getAllUsers,
+  getUserById
+};
--- back-end/routes/route.js
+++ back-end/routes/route.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/controller');
+const controller = require("../controllers/controller");
 
 //Get all students
-router.get('/main', controller.getAllStudents);
-router.get('/getstuendid/:id', controller.getStudentById);
-router.post('/newstudent', controller.createStudent);
-router.put('/updatestudent/:id', controller.updateStudent);
-router.delete('/deletestudent/:id', controller.deleteStudent);
-router.get('/users', controller.getAllUsers);
-
+router.get("/main", controller.getAllStudents);
+router.get("/getstuendid/:id", controller.getStudentById);
+router.post("/newstudent", controller.createStudent);
+router.put("/updatestudent/:id", controller.updateStudent);
+router.delete("/deletestudent/:id", controller.deleteStudent);
+router.get("/users", controller.getAllUsers);
+router.get("/getUserById/:id", controller.getUserById);
 
 module.exports = router;
