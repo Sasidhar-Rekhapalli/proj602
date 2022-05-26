b092cefc8bd8834cf6eefb280025c1c6be3df565
sasidhar_R <rekhapalli5949@saskpolytech.ca>
2022-05-26 16:17:11 -0600

    added partial viewable function and documentation for addstudent and adduser page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,21 @@ import NavBar from "./components/NavBar";
 import AddUser from "./components/AddUser";
 import AddStudent from "./components/AddStudent";
 import UserManagement from "./components/UserManagement";
+import Reset from "./pages/RessetPassword";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <AddStudent />
+      <Routes>
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/addstudent" element={<AddStudent />} />
+        <Route path="/usermanagement" element={<UserManagement />} />
+        <Route path="/adduser" element={<AddUser />} />
+      </Routes>
     </Router>
   );
 }
--- front-end/src/components/AddStudent.jsx
+++ front-end/src/components/AddStudent.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import { Form, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+// React function component
 export default function AddStudent() {
   return (
     <>
+      {/* Card with title */}
       <Card.Title className="mt-3 mb-3 text-center">Add New Student</Card.Title>
+      {/* Card */}
       <Card className="mx-auto" style={{ width: "900px" }}>
         <Card.Body style={{ padding: "1rem 2rem" }}>
+          {/* Form  */}
           <Form>
             <Form.Group className="row mb-3">
+              {/* Prospective Student Checkbox  */}
               <div className=" col">
                 <input
                   type="checkbox"
@@ -17,12 +23,14 @@ export default function AddStudent() {
                 />
                 <label className="col">Prospective Student</label>
               </div>
+              {/* Student ID label with input  */}
               <Form.Label className="col-sm-2" style={{ textAlign: "right" }}>
                 Student ID:
               </Form.Label>
               <Form.Control className="col pt-0"></Form.Control>
             </Form.Group>
 
+            {/* First and Last name with input and label  */}
             <Form.Group>
               <div className="row mb-3">
                 <Form.Label className="col-md-2">First name:</Form.Label>
@@ -31,6 +39,7 @@ export default function AddStudent() {
                 <Form.Control className="col"></Form.Control>
               </div>
 
+              {/* Check box for Gender  */}
               <div className="row mb-3">
                 <Form.Label className="col-lg-2">Gender:</Form.Label>
                 <div className="form-check col-lg-1">
@@ -51,19 +60,23 @@ export default function AddStudent() {
                   />
                   <label className="form-check-label col">Female</label>
                 </div>
+                {/* Date of Birth with input and label  */}
                 <Form.Label className="col-lg-2" style={{ textAlign: "right" }}>
                   Day of Birth:
                 </Form.Label>
                 <Form.Control type="date" className="col pt-0"></Form.Control>
               </div>
 
+              {/* Country input and label  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Country:</Form.Label>
                 <Form.Control className="col"></Form.Control>
+                {/* SaskPolytech email with input and label  */}
                 <Form.Label className="col">Saskpolytech E-mail:</Form.Label>
                 <Form.Control className="col"></Form.Control>
               </div>
 
+              {/* Campus Select with options and label  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Campus:</Form.Label>
                 <Form.Select className="col">
@@ -71,6 +84,7 @@ export default function AddStudent() {
                   <option>Option 1</option>
                   <option>Option 2</option>
                 </Form.Select>
+                {/* Academic period with options and label  */}
                 <Form.Label className="col">Academic Period:</Form.Label>
                 <Form.Select className="col">
                   <option>Select Period</option>
@@ -79,6 +93,7 @@ export default function AddStudent() {
                 </Form.Select>
               </div>
 
+              {/* Year and Program with label and input  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Year:</Form.Label>
                 <Form.Control className="col"></Form.Control>
@@ -86,6 +101,7 @@ export default function AddStudent() {
                 <Form.Control className="col"></Form.Control>
               </div>
 
+              {/* Degree with options and label  */}
               <div className="row">
                 <Form.Label className="col-md-2">Degree:</Form.Label>
                 <Form.Select className="col">
@@ -93,9 +109,13 @@ export default function AddStudent() {
                   <option>Option 1</option>
                   <option>Option 2</option>
                 </Form.Select>
+
+                {/* Graduate Ind with label and input  */}
                 <Form.Label className="col" style={{ textAlign: "right" }}>
                   Graduate Ind:
                 </Form.Label>
+
+                {/* Enroll with label and input  */}
                 <Form.Control className="col"></Form.Control>
                 <Form.Label className="col" style={{ textAlign: "right" }}>
                   Enroll:
@@ -104,9 +124,14 @@ export default function AddStudent() {
               </div>
             </Form.Group>
           </Form>
-          <Button className="btn btn-primary mt-3" style={{ float: "right" }}>
-            Save new student
-          </Button>
+
+          {/* Link to go to student page  */}
+          <Link to="/student">
+            {/* Button to save new student  */}
+            <Button className="btn btn-primary mt-3" style={{ float: "right" }}>
+              Save new student
+            </Button>
+          </Link>
         </Card.Body>
       </Card>
     </>
