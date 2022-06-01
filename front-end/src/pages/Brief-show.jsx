c064088dd15139f0013f8ea422c15ec944aaac28
Nhat NGUYEN <nhatnguyen4794@gmail.com>
2022-06-01 02:59:35 -0600

    studentlist, studentinfobyID

--- front-end/src/pages/Brief-show.jsx
+++ front-end/src/pages/Brief-show.jsx
@@ -4,14 +4,17 @@ import { ConversationList } from '../components';
 import { Form, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import {Navbar,FootNav} from "../components"
+import { format } from 'date-fns';
 
-class BriefShow extends Component{
+
+class BriefShowStudent extends Component{
     //Create the form loaded student info into apporiate box
     constructor(props) {
         
         super(props)
         this.state = {
             id: this.props.match.params.id,
+            student:[],
             studentID: '',
             firstname: '',
             lastname: '',
@@ -32,27 +35,37 @@ class BriefShow extends Component{
     }
     componentDidMount = async () => {
         const {id} = this.state;
-        const student = await apis.getStudentById(id);
-
+       await apis.getStudentById(id)
+                .then(
+                  student=>{
+                    this.setState({
+            student:student.data.data,
+            studentID: student.data.data[0].std_id,
+            firstname: student.data.data[0].first_name,
+            lastname: student.data.data[0].last_name,
+            dayofbirth: student.data.data[0].birthdate,
+            saskemail: student.data.data[0].email,
+            gender: student.data.data[0].gender,
+            country: student.data.data[0].country,
+            program:student.data.data[0].program,
+            period: student.data.data[0].academic_period,
+            campus: student.data.data[0].campus,
+            degree: student.data.data[0].degree,
+            year: student.data.data[0].year,
+            graduate: student.data.data[0].graudate_ind,
+            enroll: student.data.data[0].enroll,
+                    })
+                  }
+                )
+          console.log(this.state.student);
+          console.log(this.state.studentID);
+          console.log(this.state.firstname);
+          console.log(id)
         // use api call to retrieve doccument here
-        this.setState({ // once an album is returned, set the fields to it
-            studentID: student.data.data.student_id,
-            firstname: student.data.data.first_name,
-            lastname: student.data.data.last_name,
-            dayofbirth: student.data.data.birthdate,
-            saskemail: student.data.data.email,
-            gender: student.data.data.gender,
-            country: student.data.data.country,
-            program:student.data.data.program,
-            period: student.data.data.academic_period,
-            campus: student.data.data.campus,
-            degree: student.data.data.degree,
-            year: student.data.data.year,
-            graduate: student.data.data.graudate_ind,
-            enroll: student.data.data.enroll,
-        });
+     
     }
     render(){
+      
          return(
     <>
         <Navbar/>
@@ -68,16 +81,16 @@ class BriefShow extends Component{
               <Form.Label className="col-md-2">
                 Student ID:
               </Form.Label>
-              <Form.Control className="col"></Form.Control>
+              <Form.Control className="col" value={this.state.studentID}></Form.Control>
             </Form.Group>
 
             {/* First and Last name with input and label  */}
             <Form.Group>
               <div className="row mb-3">
                 <Form.Label className="col-md-2">First name:</Form.Label>
-                <Form.Control className="col"></Form.Control>
-                <Form.Label className="col-md-2">Last name:</Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control className="col" value={this.state.firstname}></Form.Control>
+                <Form.Label className="col-md-2" >Last name:</Form.Label>
+                <Form.Control className="col" value={this.state.lastname}></Form.Control>
               </div>
 
               {/* Check box for Gender  */}
@@ -89,6 +102,8 @@ class BriefShow extends Component{
                     type="radio"
                     name="flexRadioDefault"
                     id="flexRadioDefault1"
+                    value="Male"
+                    checked={this.state.gender==="M"}
                   />
                   <label className="form-check-label col">Male</label>
                 </div>
@@ -98,6 +113,8 @@ class BriefShow extends Component{
                     type="radio"
                     name="flexRadioDefault"
                     id="flexRadioDefault2"
+                    value="Female"
+                    checked={this.state.gender==="F"}
                   />
                   <label className="form-check-label col">Female</label>
                 </div>
@@ -105,63 +122,76 @@ class BriefShow extends Component{
                 <Form.Label className="col-lg-2" style={{ textAlign: "right" }}>
                   Day of Birth:
                 </Form.Label>
-                <Form.Control type="date" className="col pt-0"></Form.Control>
+                <Form.Control type="text" className="col pt-0" value={this.state.dayofbirth}></Form.Control>
               </div>
 
               {/* Country input and label  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Country:</Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control className="col" value={this.state.country}></Form.Control>
                 {/* SaskPolytech email with input and label  */}
                 <Form.Label className="col">Saskpolytech E-mail:</Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control type="email" className="col" value={this.state.saskemail}></Form.Control>
               </div>
 
               {/* Campus Select with options and label  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Campus:</Form.Label>
                 <Form.Select className="col">
-                  <option>Select Campus</option>
-                  <option>Option 1</option>
-                  <option>Option 2</option>
+                  <option 
+                  value="Moose Jaw"
+                  checked={this.state.gender==="Moose Jaw"}>Moose Jaw</option>
+                  <option 
+                  value="Saskatoon"
+                  checked={this.state.gender==="Saskatoon"}>Saskatoon</option>
+                  <option 
+                  value="Regina"
+                  checked={this.state.gender==="Regina"}>Regina</option>
+                  <option 
+                  value="Prince Albert"
+                  checked={this.state.gender==="Prince Albert"}>Prince Albert</option>
                 </Form.Select>
                 {/* Academic period with options and label  */}
                 <Form.Label className="col">Academic Period:</Form.Label>
-                <Form.Select className="col">
-                  <option>Select Period</option>
-                  <option>Option 1</option>
-                  <option>Option 2</option>
-                </Form.Select>
+                <Form.Control type="text" className="col" value={this.state.period}></Form.Control>
+                  
               </div>
 
               {/* Year and Program with label and input  */}
               <div className="row mb-3">
                 <Form.Label className="col-md-2">Year:</Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control type="text" className="col" value={this.state.year}></Form.Control>
                 <Form.Label className="col">Program:</Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control type="text" className="col" value={this.state.program}></Form.Control>
               </div>
 
               {/* Degree with options and label  */}
               <div className="row">
                 <Form.Label className="col-md-2">Degree:</Form.Label>
-                <Form.Select className="col">
-                  <option>Select Degree</option>
-                  <option>Option 1</option>
-                  <option>Option 2</option>
+                <Form.Select className="col" aria-readonly >
+                <option 
+                  value="DIPC"
+                  checked={this.state.degree==="DIPC"}>DIPC</option>
+                  <option 
+                  value="PGCERT"
+                  checked={this.state.gender==="PGCERT"}>PGCERT</option>
+                  <option 
+                  value="CERT"
+                  checked={this.state.gender==="CERT"}>CERT</option>
+                  
                 </Form.Select>
 
                 {/* Graduate Ind with label and input  */}
                 <Form.Label className="col" style={{ textAlign: "right" }}>
                   Graduate Ind:
                 </Form.Label>
-
+                <Form.Control type="text" className="col" value={this.state.graduate}></Form.Control>
                 {/* Enroll with label and input  */}
-                <Form.Control className="col"></Form.Control>
+              
                 <Form.Label className="col" style={{ textAlign: "right" }}>
                   Enroll:
                 </Form.Label>
-                <Form.Control className="col"></Form.Control>
+                <Form.Control type="text" className="col" value={this.state.enroll}></Form.Control>
               </div>
             </Form.Group>
           </Form>
@@ -181,4 +211,4 @@ class BriefShow extends Component{
     </>
     )}
 }
-export default BriefShow;
+export default BriefShowStudent;
