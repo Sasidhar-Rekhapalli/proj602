3cf923835a5df29a2be2c72df1729ad294fe9978
darthmoy <melgarejoaguilar5636@saskpolytech.ca>
2022-06-10 11:20:22 -0600

    Added coments to index from App and Components

--- front-end/src/app/index.js
+++ front-end/src/app/index.js
@@ -7,17 +7,36 @@ class App extends Component{
     render(){
         return(
             <Router>
+                    {/*This template will contain forms to insert data and tables to show data.
+                        Every single component has its own implementation.
+                        If there is a situation to add more components or remove components
+                    */}
                     <Switch>
+                    {/* Component that Shows loging page */}
                     <Route exact path="/" extract component={LoginView}/>
                    
+                   {/* Component that Shows the detailed student’s information */}
                     <Route path="/isms/briefshow/:id" extract component={BriefShowStudent}/>
                     
+                    {/* Component that Shows the form to input user’s information data */}
                     <Route path="/isms/register" extract component={RegisterUser}/>
+
+                    {/* Component that Shows the students’ basic information in a table with searching operation. This page has two options,  to add new student or setup user’s profiles (applicable to Admin). */}
                     <Route path="/isms/studentpage" extract component={StudentPage}/>
+
+                    {/* Component that Shows the users’ basic information in a table. Option to add new User or Update user’s role. This page is applicable to Admin  */}
                     <Route path="/isms/usermanagement" extract component={UserManagement}/>
+
+                    {/* Component that Shows a form to input user’s information data. */}
                     <Route path="/isms/addstudent" extract component={AddStudent}/>
+
+                    {/* Component that Show a forms to add conversation to students */}
                     <Route path="/isms/addnote/:id" extract component={AddConversation}/>
+
+                    {/* Component that Shows a form to reset password for users. */}
                     <Route path="/isms/resetpassword" extract component={ResetPassword}/>
+
+                    {/* Component that shows a table with Detailed dialog between Students and Users */}
                     <Route path="/isms/detaildialog/:id" extract component={DetailDialog}/>
                     
                     </Switch>
